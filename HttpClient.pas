2022981b4e066007c0ec714ca1e071265f4bb71c
isuru_s <rmx.sas@gmail.com>
2022-02-18 15:40:06 +0530

    Post, Put request functions

--- HttpClient.pas
+++ HttpClient.pas
@@ -18,11 +18,18 @@ type
     HTTPRequest: TNetHTTPRequest;
     BitBtn1: TBitBtn;
     BtnGet: TButton;
-    Button1: TButton;
+    BtnPost: TButton;
+    BtnPut: TButton;
+    BtnDel: TButton;
+    BodyMemo: TMemo;
+    BtnResetBody: TButton;
+    BtnResetResponse: TButton;
     procedure BtnGetClick(Sender: TObject);
     procedure BtnPostClick(Sender: TObject);
     procedure BtnPutClick(Sender: TObject);
     procedure BtnDeleteClick(Sender: TObject);
+    procedure BtnResetResponseClick(Sender: TObject);
+    procedure BtnResetBodyClick(Sender: TObject);
     procedure HTTPRequestRequestCompleted(const Sender: TObject;
       const AResponse: IHTTPResponse);
   private
@@ -40,26 +47,50 @@ implementation
 {______________________________________________________________________________}
 {______________________________________________________________________________}
 {______________________________________________________________________________}
-procedure TForm1.BtnGetClick(Sender: TObject);                                    // GET Click
+// Get Click
+procedure TForm1.BtnGetClick(Sender: TObject);
 begin
   HTTPRequest.MethodString:= 'GET'; // HTTP Method.
   HTTPRequest.URL:= EdtUrl.Text;    // Assign the URL
   HTTPRequest.Execute();            // execute
 end;
-{______________________________________________________________________________}  // POST Click
+{______________________________________________________________________________}
+// Post Click
 procedure TForm1.BtnPostClick(Sender: TObject);
+Var
+  IContentStream: TStringStream;
 begin
-
+  // Get Memo input -> encode into utf8, then assign into IContentStream
+  IContentStream:= TStringStream.Create(BodyMemo.Lines.Text, TEncoding.UTF8);
+  IContentStream.Seek(0,TSeekOrigin.soBeginning);
+  HTTPRequest.Post(EdtUrl.Text, IContentStream, nil, nil);
 end;
-{______________________________________________________________________________}  // PUT Click
+{______________________________________________________________________________}
+// Put Click
 procedure TForm1.BtnPutClick(Sender: TObject);
+Var
+  IContentStream: TStringStream;
 begin
-
+  IContentStream:= TStringStream.Create(BodyMemo.Lines.Text, TEncoding.UTF8);
+  IContentStream.Seek(0,TSeekOrigin.soBeginning);
+  HTTPRequest.Put(EdtUrl.Text, IContentStream, nil, nil);
 end;
-{______________________________________________________________________________}  // DELETE Click
+{______________________________________________________________________________}
+// Delete Click
 procedure TForm1.BtnDeleteClick(Sender: TObject);
 begin
 
+end;
+{______________________________________________________________________________}
+// Reset Body
+procedure TForm1.BtnResetBodyClick(Sender: TObject);
+begin
+  BodyMemo.Lines.Text:=EmptyStr;
+end;
+// Reset Response
+procedure TForm1.BtnResetResponseClick(Sender: TObject);
+begin
+  ResponseMemo.Lines.Text:=EmptyStr;
 end;
 {______________________________________________________________________________}
 procedure TForm1.HTTPRequestRequestCompleted(const Sender: TObject;
